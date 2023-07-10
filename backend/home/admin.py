1f0477b8286f886ed1be5abb84e638c4a15bb8e9
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-10 02:51:53 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Chia,Hummus,Rojo
+admin.site.register(Rojo)
+admin.site.register(Chia)
+admin.site.register(Hummus)
 
 # Register your models here.
