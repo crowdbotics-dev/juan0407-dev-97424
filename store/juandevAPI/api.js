0b49a01d10411077f0e964eb3ef5986e7f58ea4f
Crowdbotics <engineering@crowdbotics.com>
2023-07-10 03:06:21 +0000

    Update store.

--- store/juandevAPI/api.js
+++ store/juandevAPI/api.js
@@ -24,44 +24,8 @@ function api_v1_chia_partial_update(payload) {
 function api_v1_chia_destroy(payload) {
   return juandevAPI.delete(`/api/v1/chia/${payload.id}/`)
 }
-function api_v1_hummus_list(payload) {
-  return juandevAPI.get(`/api/v1/hummus/`)
-}
-function api_v1_hummus_create(payload) {
-  return juandevAPI.post(`/api/v1/hummus/`, payload)
-}
-function api_v1_hummus_retrieve(payload) {
-  return juandevAPI.get(`/api/v1/hummus/${payload.id}/`)
-}
-function api_v1_hummus_update(payload) {
-  return juandevAPI.put(`/api/v1/hummus/${payload.id}/`, payload)
-}
-function api_v1_hummus_partial_update(payload) {
-  return juandevAPI.patch(`/api/v1/hummus/${payload.id}/`, payload)
-}
-function api_v1_hummus_destroy(payload) {
-  return juandevAPI.delete(`/api/v1/hummus/${payload.id}/`)
-}
 function api_v1_login_create(payload) {
   return juandevAPI.post(`/api/v1/login/`, payload)
-}
-function api_v1_rojo_list(payload) {
-  return juandevAPI.get(`/api/v1/rojo/`)
-}
-function api_v1_rojo_create(payload) {
-  return juandevAPI.post(`/api/v1/rojo/`, payload)
-}
-function api_v1_rojo_retrieve(payload) {
-  return juandevAPI.get(`/api/v1/rojo/${payload.id}/`)
-}
-function api_v1_rojo_update(payload) {
-  return juandevAPI.put(`/api/v1/rojo/${payload.id}/`, payload)
-}
-function api_v1_rojo_partial_update(payload) {
-  return juandevAPI.patch(`/api/v1/rojo/${payload.id}/`, payload)
-}
-function api_v1_rojo_destroy(payload) {
-  return juandevAPI.delete(`/api/v1/rojo/${payload.id}/`)
 }
 function api_v1_signup_create(payload) {
   return juandevAPI.post(`/api/v1/signup/`, payload)
@@ -107,19 +71,7 @@ export const apiService = {
   api_v1_chia_update,
   api_v1_chia_partial_update,
   api_v1_chia_destroy,
-  api_v1_hummus_list,
-  api_v1_hummus_create,
-  api_v1_hummus_retrieve,
-  api_v1_hummus_update,
-  api_v1_hummus_partial_update,
-  api_v1_hummus_destroy,
   api_v1_login_create,
-  api_v1_rojo_list,
-  api_v1_rojo_create,
-  api_v1_rojo_retrieve,
-  api_v1_rojo_update,
-  api_v1_rojo_partial_update,
-  api_v1_rojo_destroy,
   api_v1_signup_create,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
